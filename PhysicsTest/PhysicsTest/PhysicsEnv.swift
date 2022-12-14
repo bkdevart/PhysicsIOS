1c33ec2497be2e0a21902b80f7ece2c640a3a722
Brandon Knox <bknox.dev@gmail.com>
2022-12-14 16:58:58 -0700

    beginning of camera work, incredibly buggy

--- PhysicsTest/PhysicsTest/PhysicsEnv.swift
+++ PhysicsTest/PhysicsTest/PhysicsEnv.swift
@@ -28,51 +28,51 @@ class GameScene: SKScene {
     }
 
     // release
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        // TODO: use this to release object (instead of start of touch event)
-//        guard let touch = touches.first else { return }
-//        let location = touch.location(in: self)
-//
-//        switch controls.addMethod {
-//        case .clear:
-//            // select node and delete only that one
-//            let touchedNodes = nodes(at: location)
-//            controls.selectedNodes = touchedNodes
-//            // will crash here if no nodes are touched
-//            if touchedNodes.count > 0 {
-//                controls.selectedNode = touchedNodes[0]
-//            } else {
-//                // this removes the last node that was touched from being saved
-//                controls.selectedNode = SKNode()
-//            }
-//            controls.selectedNode.removeFromParent()
-//        case .add:
-//            let touchedNodes = nodes(at: location)
-//            controls.selectedNodes = touchedNodes
-//            // will crash here if no nodes are touched
-//            if touchedNodes.count > 0 {
-//                // check if selectedNode is paint node
-//                if touchedNodes[0].zPosition != -5 {
-//                    // log node so that drag motion works
-//                    controls.selectedNode = touchedNodes[0]
-//                    // if removeOn is set, clear node
-//                    if controls.removeOn {
-//                        controls.selectedNode.removeFromParent()
-//                    }
-//                } else {
-//                    // drop new one if paint node selected (can't move paint nodes)
-//                    print("You are selecting a paint node and need to drop instead")
-//                    let newNode = renderNode(location: location, hasPhysics: true)
-//                    addChild(newNode)
-//                }
-//            } else {
-//                // if no non-paint nodes are touched, then add new one
-//                let newNode = renderNode(location: location, hasPhysics: true)
-//                addChild(newNode)
-//            }
-//        case .paint:
-//            // remove paint
-//            let touchedNodes = nodes(at: location)
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // TODO: use this to release object (instead of start of touch event)
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+
+        switch controls.addMethod {
+        case .clear:
+            // select node and delete only that one
+            let touchedNodes = nodes(at: location)
+            controls.selectedNodes = touchedNodes
+            // will crash here if no nodes are touched
+            if touchedNodes.count > 0 {
+                controls.selectedNode = touchedNodes[0]
+            } else {
+                // this removes the last node that was touched from being saved
+                controls.selectedNode = SKNode()
+            }
+            controls.selectedNode.removeFromParent()
+        case .add:
+            let touchedNodes = nodes(at: location)
+            controls.selectedNodes = touchedNodes
+            // will crash here if no nodes are touched
+            if touchedNodes.count > 0 {
+                // check if selectedNode is paint node
+                if touchedNodes[0].zPosition != -5 {
+                    // log node so that drag motion works
+                    controls.selectedNode = touchedNodes[0]
+                    // if removeOn is set, clear node
+                    if controls.removeOn {
+                        controls.selectedNode.removeFromParent()
+                    }
+                } else {
+                    // drop new one if paint node selected (can't move paint nodes)
+                    print("You are selecting a paint node and need to drop instead")
+                    let newNode = renderNode(location: location, hasPhysics: true)
+                    addChild(newNode)
+                }
+            } else {
+                // if no non-paint nodes are touched, then add new one
+                let newNode = renderNode(location: location, hasPhysics: true)
+                addChild(newNode)
+            }
+        case .paint:
+            // remove paint
+            let touchedNodes = nodes(at: location)
             
 //            if touchedNodes.count > 0 {
 //                let selectedNode = touchedNodes[0]
@@ -85,10 +85,10 @@ class GameScene: SKScene {
 //                let newNode = renderNode(location: location, hasPhysics: false, zPosition: -5)
 //                addChild(newNode)
 //            }
-//        }
+        }
         // this is needed to keep track of all children objects (shape nodes)
-//        controls.gameScene = self
-//    }
+        controls.gameScene = self
+    }
     
     // drag
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -122,20 +122,20 @@ class GameScene: SKScene {
                     // store current camera position before moving on
                     let originalCam = controls.camera.position  //camera.position
                     let center = view!.center
-//                    controls.camera.position = center
+                    controls.camera.position = center
                     let xDist = location.x - center.x
                     let yDist = location.y - center.y
                     let distance = sqrt((xDist * xDist) + (yDist * yDist)) * 0.02
                     let xPos = xDist * distance * 0.08
                     let yPos = yDist * distance * 0.08
                     // TODO: disabling until stable
-//                    camera?.position.x = -xPos  // was +=
-//                    camera?.position.y = -yPos  // was -=
-//
-//                    // force unwrap here assumes the camera was created in view
-//                    print("\(camera?.position.x), \(camera?.position.y)")
-//                    controls.camera = camera!
-//                    controls.gameScene.camera?.position = (location)
+                    camera?.position.x = -xPos  // was +=
+                    camera?.position.y = -yPos  // was -=
+
+                    // force unwrap here assumes the camera was created in view
+                    print("\(camera?.position.x), \(camera?.position.y)")
+                    controls.camera = camera!
+                    controls.gameScene.camera?.position = (location)
                 }
             }
         case .paint:
